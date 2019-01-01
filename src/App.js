120ced413cef3ee63cf97d781f2e8a7df73ebe72
gchan82 <42255277+gchan82@users.noreply.github.com>
2019-01-01 14:34:08 -0800

    11(26:07) Add Prompt.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, NavLink, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, NavLink, Redirect, Prompt} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
 // match.params.user: unclear
-const User = (params) => {
+const User = ({username}) => {
   return(
     <div>
-      <h1> Welcome, {params.username}! </h1>
+      <h1> Welcome, {username}! </h1>
     </div>
   )
 }
@@ -31,6 +31,16 @@ class App extends Component {
       <li><NavLink exact to="/user/John" activeStyle={{color: 'green'}}>John</NavLink></li>
       <li><NavLink exact to="/user/Joe" activeStyle={{color: 'green'}}>Joe</NavLink></li>
     </ul>
+
+    <Prompt
+when={!this.state.loggedIn}
+message={(location) => {
+  return location.pathname.startsWith('/user') ? 'You are not logged in. Please login to access.' : true
+}}
+
+    />
+
+    
   
 <input type="button" value={this.state.loggedIn ? 'log out' : 'login'} onClick={this.handleClick}></input>
 
@@ -42,8 +52,8 @@ class App extends Component {
       )
     }} />
     <Route exact path="/about" render={() => {return <h1>About Page</h1>}} />
-    <Route exact path="/user/:username" render={({match}) => (
-      this.state.loggedIn ? <User username={match.params.username} /> : <Redirect to="/" />
+    <Route exact path="/user/:id" render={({match}) => (
+      this.state.loggedIn ? <User username={match.params.id} /> : <Redirect to="/" />
     )}></Route>
   </div>
 </Router>
